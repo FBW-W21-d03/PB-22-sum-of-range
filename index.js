766727eecdd9a788f56b841dc3dcd321f4dca2fc
sprotte <metatron1972@live.de>
2021-11-25 10:33:24 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let zahl = [];
+let ausgabe = 2;
+
+
+
+while(zahl.length <= 100); {
+    zahl.push(ausgabe);
+    ausgabe+=2;
+    if(zahl.length == 100);
+    }
+console.log("ausgabe",zahl + ausgabe);     
