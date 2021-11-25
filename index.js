24802e111336a36a2cf68d3e9f15f6ed866c8e65
sprotte <metatron1972@live.de>
2021-11-25 11:59:19 +0100

    sprotte

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
+/*
 function ausgabe() {
-    let zahlen = 1;
+    let zahlen = 0;
     let alleZahlen = 1; 
 
-    while (ausgabe < 100) {
+    while (ausgabe <= 100) {
         alleZahlen = alleZahlen + zahlen;
         zahlen = zahlen + 1;
     }
 
     return alleZahlen;
 }
-let ergebnis = ausgabe();
+let ausgabe = ausgabe();
 console.log("summe");
+*/
+
+function product() {
+    let counter = 0;
+    let produktAllerZahlen = 1;
+
+    while (counter <= 100) {
+        produktAllerZahlen = produktAllerZahlen + counter;
+        counter = counter + 1;
+    }
+
+    return produktAllerZahlen;
+}
+let ergebnis = product();
+console.log(ergebnis);
+
+//----------------------
+function product1() {
+    let counter = 1;
+    let produktAllerZahlen = 1; 
+
+    while (counter <= 500) {
+        produktAllerZahlen = produktAllerZahlen + counter;
+        counter = counter + 1;
+    }
+
+    return produktAllerZahlen;
+}
+let ergebnis1 = product1();
+console.log(ergebnis1);
