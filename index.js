38154f0f7a883157ec920944c152e5735a9947d9
sprotte <metatron1972@live.de>
2021-11-25 11:01:57 +0100

    sprotte

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function ausgabe() {
     let zahlen = 1;
     let alleZahlen = 1; 
 
-    while (ausgabe <= 100) {
+    while (ausgabe < 100) {
         alleZahlen = alleZahlen + zahlen;
         zahlen = zahlen + 1;
     }
