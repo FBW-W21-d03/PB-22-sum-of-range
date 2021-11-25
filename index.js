5d0454ba674662284953c1332d1803cfa6160acb
ShiinyFlakes <mago_elm@hotmail.com>
2021-11-25 11:55:17 +0100

    revised

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
 function product() {
     let counter = 0;
     let produktAllerZahlen = 0;
-
     while (counter <= 100) {
         produktAllerZahlen = produktAllerZahlen + counter;
         counter = counter + 1;
     }
-
     return produktAllerZahlen;
 }
-
 let ergebnis = product();
-
 console.log(ergebnis);
 
 
 
-
-function productzwei() {
+function productzwei(wert, wert) {
+    let zahl = 0;
     let counter = 1;
-    let zahl= 0;
-    while (counter <= zahl) {
-        zahl = zahl + counter;
-        counter = counter + 1;
+
+    while (zahl <= wert) {
+        counter = counter + zahl;
+        zahl = zahl + 1;
     }
-    return zahl;
+    return counter;
 }
 
-
-let ergebniszwei = productzwei(150, 200);
-
+let ergebniszwei = productzwei(777, 500);
 console.log(ergebniszwei);
+
