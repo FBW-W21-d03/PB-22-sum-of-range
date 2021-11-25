ad7ea065b8e1fb4c64d70d799e8f31310e94080e
slavablack <veaceslav.ciornii@magenta.de>
2021-11-25 13:20:31 +0100

    Teil 2

--- hundert.js
+++ hundert.js
@@ -1,11 +1,25 @@
-function hundert() {
-    let number=1;
-    let results=0;
-    while (number<=100) {
-        results=results+number;
-        number++;
-    }
-    return results;
-}
-let meinresults=hundert();
-console.log (meinresults);
+// function hundert() {
+//     let number=1;
+//     let results=0;
+//     while (number<=100) {
+//         results=results+number;
+//         number++;
+//     }
+//     return results;
+// }
+// let meinresults=hundert();
+// console.log (meinresults);
+
+console.log("--------------------------------------------------");
+ function hundert(ersteZahl,zweitZahl) {
+
+   let unu=ersteZahl;
+       let doi=0;
+        while (unu<=zweitZahl) {
+           doi=doi+unu;
+         unu++;
+        }
+        return unu;
+ }
+let unuplusdoi=hundert(100,200);
+console.log(unuplusdoi);
